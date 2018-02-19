c509b79e1ef69e69b828f6bda91db72f41833c88
Muhammad Satria Wibawa_1202154224 <35917248+msatriawibawa@users.noreply.github.com>
2018-02-19 14:13:14 +0700

    Update Splash.java

--- app/src/main/java/com/example/android/MuhammadSatriaWibawa_1202154224_Modul2/Splash.java
+++ app/src/main/java/com/example/android/MuhammadSatriaWibawa_1202154224_Modul2/Splash.java
@@ -10,18 +10,6 @@ public class Splash extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
-        Toast.makeText(this, "MuhammadSatriaWibawa_1202154224_Modul2", Toast.LENGTH_SHORT).show(); //akan menampilkan pesan RIZKY ALAMSYAH_1202154185
-        final Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
-
-            // method untuk mengarahkan ke aktivitas selanjutnya
-            @Override
-            public void run() {
-                startActivity(new Intent(getApplicationContext(), MainActivity.class));
-                finish();
-            }
-        }, 4000L); //4000 L = 4 detik
+        
     }
 }
